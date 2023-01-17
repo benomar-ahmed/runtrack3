aab5678765f58c6ce24e794ed9aee306788fd088
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2023-01-17 09:27:35 +0100

    Move folder in foler Jour01

---
+++ Jour01/Job 07/script.js
@@ -0,0 +1,29 @@
+function jourtravaille(date)
+{
+
+    let jour = get.Date();
+    let semaine = get.Day();
+    let mois = get.Month();
+    let year = get.FullYear();
+
+    const jourferie = [new Date(2020, 1, 1),
+    new Date(2020, 4, 12),
+    new Date(2020, 4, 13),
+    new Date(2020, 5, 1),
+    new Date(2020, 5, 8),
+    new Date(2020, 5, 21),
+    new Date(2020, 5, 31),
+    new Date(2020, 6, 1),
+    new Date(2020, 7, 14),
+    new Date(2020, 8, 15),
+    new Date(2020, 11, 1),
+    new Date(2020, 11, 11),
+    new Date(2020, 12, 25)]
+
+    let estferie = console.log(jourferie)
+
+
+    
+}
+
+
