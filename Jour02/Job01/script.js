581baea1de74dac4068b6e18c18069217a757b70
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2023-01-17 10:58:51 +0100

    update script.js Job 01

--- Jour02/Job01/script.js
+++ Jour02/Job01/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+
     function citation(){
         const article = document.getElementById('citation').innerHTML;
         console.log(article);
     }
     const button1 = document.querySelector("#button");
-    button1.addEventListener("click",citation());
+    button1.addEventListener("click",citation);
 
 });
